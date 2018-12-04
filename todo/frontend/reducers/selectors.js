f4bd890a6a1af7bedeb63dd4a30f5ea869e52d10
ali alkaheli <alialkaheli@alis-MacBook-Pro.local>
2018-12-03 16:28:08 -0800

    selectors for allTodos function

---
+++ todo/frontend/reducers/selectors.js
@@ -0,0 +1,4 @@
+const allTodos = ({state}) => {
+    Object.keys(state.todos).map(id => state.todos[id]);
+}
+export default allTodos;
--- todo/todo_redux.jsx
+++ todo/todo_redux.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import configStore from './frontend/store/store';
 import { receiveTodos, receiveTodo } from './frontend/actions/todo_actions'
 import Root from './frontend/components/root';
+import allTodos from './frontend/reducers/selectors'
 
 const store = configStore();
 window.store = store;
 window.receiveTodo = receiveTodo;
 window.receiveTodos = receiveTodos;
+window.allTodos = allTodos;
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('content');
